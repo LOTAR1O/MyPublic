8ec804967ed40c4ee5b93f2587171a1f96ca3b6e
Vovanti Home <lotario.beta@yandex.ru>
2025-12-01 21:10:15 +0300

    Скрипт на ввод логина и пароля 2.0

--- Bash scripts/login_pass.sh
+++ Bash scripts/login_pass.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
-# shellcheck disable=SC2034
+# Заправшивает ввод пользовтаелем логина.
 read -p "Введите логин: " user_login
-read -s -p "Введите пароль: " user_password
 
+# Максимальное кол-во попыток ввода пароля
+max_attempts=3
+# Счетчик попыток ввода начианается с нуля.
+attempts=0
 
-if [ "$user_password" = "pass123" ]; then
-    echo ""
-    echo "Вход выполнен."
-else
-    echo""
-    echo "Введен неправильный пароль."
-fi
+#  Основной цикл проверки
+while [ $attempts -lt $max_attempts ]; do
+    read -s -p "Введите пароль: " user_password
+    echo # Перенос строки после скрытого ввода
+
+    if [ "$user_password" = "pass123" ]; then
+        echo"" "Вход выполнен."
+        exit 0 # Вход выполнен успешно
+    else
+        attempts=$((attempts += 1))
+        remaining=$((max_attempts - attempts))
+        if [ $remaining -gt 0 ]; then
+            echo "Введен неправильный пароль. Осталось попыток: $remaining"
+        else
+            echo "Превышено число попыток. Доступ запрещен."
+            exit 1 # Ошибка
+        fi
+    fi
+done
